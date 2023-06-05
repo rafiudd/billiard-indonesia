2f82fbf3efbdc7be93482e41a1dcc944ec4e1767
permanaa <irmanpermana.lu@gmail.com>
2023-06-05 16:16:03 +0700

    setting prisma

--- README.md
+++ README.md
@@ -2,14 +2,18 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+- First, setting up env
+
+- instal dependencies:
+
+```bash
+npm install
+```
+
+- run the development server:
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import prisma from '@/lib/prisma'
 
-export default function Home() {
+export default async function Home() {
+  const result = await prisma?.vehicle?.findMany() || []
+  console.log(result)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
