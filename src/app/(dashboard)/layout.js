f0ef2a1784a02fecb1bd8e4c960a93e3cdf06837
permanaa <irmanpermana.lu@gmail.com>
2023-06-17 15:28:44 +0700

    unauthenticated redirect login

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,10 +1,15 @@
 import Navbar from '@/components/Navbar'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default function DashboardLayout({ children }) {
   const cookieStore = cookies()
   const isLogin = cookieStore.get('isLogin')
   const id = cookieStore.get('id')
+
+  if (!isLogin) {
+    redirect('/login')
+  }
 
   return (
     <div>
